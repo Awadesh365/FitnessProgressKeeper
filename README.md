eb57422cd24b0d127ed2554733b27986612e57a7
awadesh <36awadesh@gmail.com>
2023-09-18 02:08:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fitness Progress Keeper
+
+This website is Live at :- https://incandescent-croissant-5d868e.netlify.app/
+
+The Fitness Progress Keeper is a versatile and motivating fitness tracking application. It empowers users to monitor their fitness journey, record completed workouts, and celebrate their accomplishments. Whether you're conquering daily challenges, achieving workout milestones, or striving for a healthier lifestyle, this app is your dedicated fitness companion.
+
+**Key Features:**
+
+- **Challenge Tracking:** Log your completed fitness challenges and workouts effortlessly.
+- **Achievement Records:** Keep a detailed record of each challenge, including exercise type, duration, and date.
+- **Visual Progress:** Monitor your fitness progress over time with an intuitive and easy-to-read interface.
+- **Goal Setting:** Set and track fitness goals to stay motivated and focused on your objectives.
+- **Motivational Insights:** Receive inspirational fitness quotes and tips to keep you motivated on your journey.
+- **Share Your Success:** Share your fitness achievements and milestones with friends and the community.
+
+Stay committed to your fitness goals, track your accomplishments, and witness your progress with the Fitness Progress Keeper. Start your fitness journey today!
---
+++ app.js
@@ -0,0 +1,72 @@
+/*
+    Awadesh Nautiyal
+*/
+
+//-----------PeopleCounter Logic-------------
+
+// Variables to store references to HTML elements
+let countEl, saveEl, totalEl;
+
+// Function to initialize references to HTML elements
+function essentialFunction() {
+  countEl = document.getElementById("count-el");
+  saveEl = document.getElementById("save-el");
+  totalEl = document.getElementById("total-el");
+}
+
+// Initialize counters for total and current count
+let total = 0,
+  count = 0;
+
+// Function to increment the count
+function increment() {
+  essentialFunction();
+
+  // Increment both current and total counts
+  count++;
+  total++;
+
+  // Update the displayed count
+  countEl.textContent = count;
+}
+
+// Function to save the current count
+function save() {
+  essentialFunction();
+
+  // Format the current count as previous entry
+  let previousEntries = " " + count + " " + " - ";
+
+  // Append the previous entry to the list
+  saveEl.textContent += previousEntries;
+
+  // Log the total count to the console
+  console.log(total);
+
+  // Reset the current count and update the displayed count
+  count = 0;
+  countEl.textContent = 0;
+}
+
+// Function to show the total count
+function showTotal() {
+  essentialFunction();
+
+  // Update the displayed total count
+  totalEl.textContent = "Total Challenges Completed:-" + total;
+
+  // Reset the current count and update the displayed count and entries
+  count = 0;
+  countEl.textContent = 0;
+  saveEl.textContent = "Challenges Accomplished:";
+}
+
+// Function to reset and start from scratch
+function letStartFromScratch() {
+  // Reset current and total counts, and update displayed elements
+  count = 0;
+  countEl.textContent = 0;
+  saveEl.textContent = "Challenges Accomplished:";
+  total = 0;
+  totalEl.textContent = "Total Challenges Completed:-";
+}
